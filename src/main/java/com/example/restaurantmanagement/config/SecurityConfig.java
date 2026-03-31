96a5a9bfbb001bcf2e40d168788d50c080d1a2a9
TennySon2602 <nguyenhoangson20008@gmail.com>
2026-03-31 21:19:07 +0700

    fix deploy

--- src/main/java/com/example/restaurantmanagement/config/SecurityConfig.java
+++ src/main/java/com/example/restaurantmanagement/config/SecurityConfig.java
@@ -47,8 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-        configuration.setAllowedOrigins(List.of("https://desinare.vercel.app/"));
+        configuration.setAllowedOrigins(List.of("*"));
 
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
